17b609b6208e26acaa0b6a71b0b026c36c217a32
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-12 19:18:02 +0200

    hotovo

--- bmi-input2.js
+++ bmi-input2.js
@@ -1,12 +1,23 @@
-//funkce pro vypocet bmi
-
-function vypocitejBMI (){
-    let vyskaMetry = parseFloat(document.querySelector('input[name="vyska"]').value);
+// funkce pro výpočet BMI
+function vypocitejBMI() {
+    let vyskaMetry = parseFloat(document.querySelector('input[name="vyska"]').value) / 100;
     let vahaKilogramy = parseFloat(document.querySelector('input[name="vaha"]').value);
-    let BMI = Math.round(vahaKilogramy/ Math.pow(vyskaMetry, 2));
 
+    return Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2));
+}
+
+// funkce pro zapsání BMI do UI
+function vypisBMI(BMI) {
+    document.querySelector('#vysledek > span').innerHTML = BMI;
+}
+
+function vypocitejVypisBMI() {
+    let BMI = vypocitejBMI();
+    vypisBMI(BMI);
 }
 
 document.querySelectorAll('input').forEach((element) => {
-    element.addEventListener('change', vypocitejBMI);
+    element.addEventListener('change', vypocitejVypisBMI);
 });
+
+   
