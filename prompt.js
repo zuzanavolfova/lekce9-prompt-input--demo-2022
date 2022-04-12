3734fb33d6507f2f5029ff86b352e8d507124203
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-12 18:31:13 +0200

    implement prompt

---
+++ prompt.js
@@ -0,0 +1,9 @@
+//vytvoreni promptu pro zadani jmena + prijmnei a veku a veku a naplneni do promennych
+
+let celeJmeno = prompt('zadej jmeno a prijmeni');
+let vek = prompt('Zadej vek', 18);
+
+// vypsani vysledku pomoci alert
+
+alert(`Jmenujes se ${celeJmeno} a je ti ${vek} let. `);
+
